ad32a3a071326a29bf4d9b6f6400020b7c7ed5c5
tsdmrfth <line.939018>
2019-01-27 17:04:52 +0300

    Added git flow init command

--- src/init.js
+++ src/init.js
@@ -2,11 +2,56 @@ const vscode = require('vscode');
 const checkIsCommandInstalled = require('./utils/checkIsCommandInstalled')
 const strings = require('./strings')
 const open = require('open')
+const { isEmptyString } = require('./utils/isEmptyString')
 
 const init = (context) => {
     let gfInit = vscode.commands.registerCommand('extension.gfInit', async () => {
+        let master, develop, feature, bugfix, release, hotfix, support, tagPrefix = ''
         if (await checkGF()) {
+            let result = await showInputBox(strings.firstGFInitQuestion)
+            master = returnValue(result, 'master')
+            result = await showInputBox(strings.secondGFInitQuestion)
+            develop = returnValue(result, 'develop')
+            result = await showInputBox(strings.thirdGFInitQuestion)
+            feature = returnValue(result, 'feature')
+            result = await showInputBox(strings.fourthGFInitQuestion)
+            bugfix = returnValue(result, 'bugfix')
+            result = await showInputBox(strings.fifthGFInitQuestion)
+            release = returnValue(result, 'release')
+            result = await showInputBox(strings.sixthGFInitQuestion)
+            hotfix = returnValue(result, 'hotfix')
+            result = await showInputBox(strings.seventhGFInitQuestion)
+            support = returnValue(result, 'support')
+            result = await showInputBox(strings.eighthGFInitQuestion)
+            tagPrefix = returnValue(result, '')
 
+            let terminal = vscode.window.createTerminal('BABA-Flow')
+            terminal.show()
+            terminal.sendText('git flow init')
+            terminal.onDidWriteData(data => {
+                if (data.includes('master') && this.isMasterSent) {
+                    terminal.sendText(master)
+                    this.isMasterSent = true
+                } else if (data.includes('develop') && this.isDevelopSent) {
+                    terminal.sendText(develop)
+                    this.isDevelopSent = true
+                } else if (data.includes('feature') && this.isFeatureSent) {
+                    terminal.sendText(feature)
+                    this.isFeatureSent = true
+                } else if (data.includes('bugfix') && this.isBugFixSent) {
+                    terminal.sendText(bugfix)
+                    this.isBugFixSent = true
+                } else if (data.includes('release') && this.isReleaseSent) {
+                    terminal.sendText(release)
+                    this.isReleaseSent = true
+                } else if (data.includes('hotfix') && this.isHotFixSent) {
+                    terminal.sendText(hotfix)
+                    this.isHotFixSent = true
+                } else if (data.includes('tag prefix') && this.isTagPrefixSent) {
+                    terminal.sendText(tagPrefix)
+                    this.isTagPrefixSent = true
+                }
+            })
         }
     });
 
@@ -28,6 +73,17 @@ const showLibraryNotInstalledMessage = () => {
                 open(res)
             }
         })
+}
+
+const showInputBox = async (message) => {
+    let result = await vscode.window.showInputBox({
+        placeHolder: message
+    })
+    return result
+}
+
+const returnValue = (value, defaultValue) => {
+    return isEmptyString(value) ? defaultValue : value
 }
 
 module.exports = init
--- src/strings.js
+++ src/strings.js
@@ -1,4 +1,12 @@
 module.exports = {
     libraryNotInstalled: `Git Flow library is not installed on your computer. Go to link and follow instructions to install it.`,
-    libraryInstallUrl: 'https://goo.gl/M2Ai1a'
+    libraryInstallUrl: 'https://goo.gl/M2Ai1a',
+    firstGFInitQuestion: 'Branch name for production releases: [default is master]',
+    secondGFInitQuestion: 'Branch name for "next release" development: [default is develop]',
+    thirdGFInitQuestion: 'How to name your feature branches? [default is feature/]',
+    fourthGFInitQuestion: 'How to name your bugfix branches? [default is bugfix/]',
+    fifthGFInitQuestion: 'How to name your release branches? [default is release/]',
+    sixthGFInitQuestion: 'How to name your hotfix branches? [default is hotfix/]',
+    seventhGFInitQuestion: 'How to name your support branches? [default is support/]',
+    eighthGFInitQuestion: 'How to name your version tag prefix?',
 }
